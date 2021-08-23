99f2fcd549517ab44aed63f574cdae765d8972a8
Janou Treffers <janoutreffers@hotmail.com>
2021-08-23 16:32:26 +0200

    First Commit
    Verse site

---
+++ portfolio2021/js/works.js
@@ -0,0 +1,34 @@
+// Belongs in work.js
+
+var works = [
+  {
+    title: "Website ABFF",
+    pic: "img/logoabff.jpg",
+    url: "https://amsterdamsbuurtfilmfestival2017.netlify.app/"
+  },
+  {
+    title: "Webapp/Bikeshop",
+    pic:  "img/frog.jpg",
+    url: "https://happybikeshop-1.herokuapp.com/"
+  },
+  {
+    title: "Website/Wordpress",
+    pic: "img/Esther_Hertog-min.png",
+    url: "https://esthertog.com/"
+  },
+  {
+    title: "C.A.T. Contol Systems",
+    pic: "img/slagboomwrap.png",
+    url: "https://catcontrolsystems.nl/"
+  },
+   {
+    title: "Website",
+    pic: "img/SASK_Foundation.png",
+    url: "https://stichtingsask.org/"
+  },
+   {
+    title: "Website/Blog",
+    pic: "img/blog_voorbeeld.png",
+    url: "https://happy-boyd-def846.netlify.app/"
+  },
+];
